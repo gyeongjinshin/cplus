bfb1a4d8a95615a406fdbbbf3283f2f10d185978
gyeongjin.shin <skj4140@naver.com>
2018-09-09 12:28:04 +0900

    Practice static library

---
+++ library/static/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "mysum.h"
+
+int main()
+{
+    printf("10+2 = %d\n", sum(10,2));
+    printf("10-2 = %d\n", diff(10,2));
+    
+    return 0;
+}
---
+++ library/static/mysum.c
@@ -0,0 +1,11 @@
+#include "mysum.h"
+
+int sum(int a, int b)
+{
+    return a+b;
+}
+
+int diff(int a, int b)
+{
+    return a-b;
+}
